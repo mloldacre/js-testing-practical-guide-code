16266e535d52b893becbaeb476e7c585720eac52
Michael Oldacre <65476228+mloldacre@users.noreply.github.com>
2024-01-25 03:04:34 -0500

    updated math test, trying to fix jest watch option

--- code/basic-testing-frontend/src/tests/math.spec.js
+++ code/basic-testing-frontend/src/tests/math.spec.js
@@ -2,6 +2,31 @@ import { test, it, expect } from '@jest/globals';
 import { add } from '../math';
 
 it('should sum all number values in an array', () => {
-	const result = add([1, 2, 3, 4]);
-	expect(result).toBe(10);
+	// *Arrange
+	const numbers = [1, 2, 3, 4];
+	const expectedResult = numbers.reduce((prevValue, curValue) => prevValue + curValue, 0);
+
+	// *Act
+	const result = add(numbers);
+
+	// *Assert
+	expect(result).toBe(expectedResult);
+});
+
+it('should yield NaN if at least one invalid value is provided', () => {
+	// *Arrange
+	const invalidInputs = ['invalid', 1];
+	// *Act
+	const result = add(invalidInputs);
+	// *Assert
+	expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+	// *Arrange
+	const textInputs = ['1', '2', '3'];
+	// *Act
+	const expectedResult = numbers.reduce((prevValue, curValue) => +prevValue + +curValue, 0);
+	// *Assert
+	expect(result).toBe(expectedResult);
 });
